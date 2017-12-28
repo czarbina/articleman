bbd834ec8625f6afe1845746b1b7040173dd1197
Sabrina Kesler <sabrina@getvictorious.com>
2017-12-27 22:36:20 -0800

    Yay it's updating and deleting

--- public/app.js
+++ public/app.js
@@ -33,12 +33,15 @@ $(document).on("click", "p", function() {
       $("#review").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
       $("#review").append("<button data-id='" + data._id + "' id='savereview'>Save Review</button>");
+      $("#review").append("<button data-id='" + data._id + "' id='deletereview'>Remove Review</button>");
+
 
       // If there's a note in the article
       if (data.review) {
         // Place the title of the note in the title input
+        console.log("hi");
         $("#titleinput").val(data.review.title);
-        // Place the body of the note in the body textarea
+        // // Place the body of the note in the body textarea
         $("#bodyinput").val(data.review.body);
       }
     });
@@ -66,6 +69,37 @@ $(document).on("click", "#savereview", function() {
       // Log the response
       console.log(data);
       console.log("POST should work...")
+      // Empty the notes section
+      $("#review").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
+
+$(document).on("click", "#deletereview", function() {
+  // alert("I've been clicked!");
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "DELETE",
+    url: "/albums/" + thisId,
+    // data: {
+    //   // Value taken from title input
+    //   title: $("#titleinput").val(),
+    //   // Value taken from note textarea
+    //   body: $("#bodyinput").val(),
+
+    // }
+  })
+    // With that done
+    .done(function(data) {
+      // Log the response
+      console.log(data);
+      console.log("delete this bad boy...")
       // Empty the notes section
       $("#review").empty();
     });
--- server.js
+++ server.js
@@ -113,11 +113,31 @@ app.post("/albums/:id", function(req, res) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Album.findOneAndUpdate({ _id: req.params.id }, { note: dbReview._id }, { new: true });
+      return db.Album.findOneAndUpdate({ _id: req.params.id }, { review: dbReview._id }, { new: true });
     })
     .then(function(dbAlbum) {
       // If we were able to successfully update an Article, send it back to the client
       res.json(dbAlbum);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+app.delete("/albums/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Review
+    .remove(req.body)
+    .then(function(dbReview) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      db.Album.findOneAndRemove({ _id: req.params.id }, { review: dbReview._id });
+    })
+    .then(function(dbAlbum) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.end();
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
